91f352af8e67b40698897a0f1bbb261c0eb18518
austin espinal <austinespi@gmail.com>
2021-10-31 13:45:38 -0400

    added random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -22,13 +22,16 @@ var fight = function (enemyName) {
       if (confirmSkip) {
         window.alert(playerName + " has decided to skip this fight. Good bye!");
         //subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
         console.log("playerMoney", playerMoney);
         break;
       }
     }
     // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+    //generate random damage value based on player's attack power
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+
+    enemyHealth = Math.max(0, enemyHealth - damage);
     console.log(
       playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
     );
@@ -47,7 +50,9 @@ var fight = function (enemyName) {
     }
 
     // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+    playerHealth = Math.max(0, playerHealth - damage);
     console.log(
       enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
     );
@@ -64,12 +69,21 @@ var fight = function (enemyName) {
 
 };
 
+//function to generate a random numeric value
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+  return value;
+};
+
 //Function to start a new game
 var startGame = function() {
   //resets player stats
   playerHealth = 100;
   playerAttack = 10;
   playerMoney = 10;
+
+  enemyHealth = randomNumber(40, 60);
 
   for (var i = 0; i < enemyNames.length; i++) {
     if (playerHealth > 0) {
