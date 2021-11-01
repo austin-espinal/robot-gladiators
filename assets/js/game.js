3ee0730522184c8636bf27ea22586100a58c4f5c
austin espinal <austinespi@gmail.com>
2021-11-01 01:22:21 -0400

    changed shopOption switch case to accept numerical instead of string

--- assets/js/game.js
+++ assets/js/game.js
@@ -93,7 +93,7 @@ var startGame = function() {
     if (playerInfo.health > 0) {
       //let player know what round they are in, remember that arrays start at 0 so must add 1
       window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
-      console.log(playerInfo.name);
+
       //pick new enemy to fight based on the index of the enemyNames array
     var pickedEnemyObj = enemyInfo[i];
   
@@ -153,23 +153,21 @@ var endGame = function() {
 var shop = function() {
   //ask player what they'd like to do
   var shopOptionPrompt = window.prompt(
-    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter: '1' for REFILL, '2' for UPGRADE, or '3' for LEAVE to make a choice."
   );
 
+  shopOptionPrompt = parseInt(shopOptionPrompt);
   // use switch to carry out action
 switch (shopOptionPrompt) {
-    case "REFILL":
-    case "refill":
+    case 1:
       playerInfo.refillHealth();
     break;
 
-    case "UPGRADE":
-    case "upgrade":
+    case 2:
       playerInfo.upgradeAttack();
     break;
     
-    case "LEAVE":
-    case "leave":
+    case 3:
     window.alert("Leaving the store.");
 
     // do nothing, so function will end
